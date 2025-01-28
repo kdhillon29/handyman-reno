a96a7dfee6f178ea6946d5929f4b11d322c5a2b5
kanwar Dhillon <kdhillon469@gmail.com>
2025-01-28 12:34:51 +1100

    fix layout

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,8 +4,8 @@ import logo from '../public/logo2.png'
 
 const Logo = () => {
   return (
-    <div className="flex items-center">
-      <Image src={logo} alt="logo" width={50} height={50} />
+    <div className="flex items-center ">
+      <Image src={logo} alt="logo" width={40} height={40} />
       <h1 className="text-2xl font-bold">Nbg Construction</h1>
     </div>
   );
--- modules/Footer.tsx
+++ modules/Footer.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
     <footer className="bg-neutral-900 text-white py-6 md:py-12
      border-t-1 border-neutral-700">
     <Container>
-        <div className="flex flex-col md:flex-row justify-between items-center md:items-end gap-3">
+        <div className="flex flex-col md:flex-row  px-6 justify-between items-center md:items-end gap-3">
             
             <Logo/>
-            <p className="text-xs text-neutral-400 font-semibold ">
+            <p className="text-xs md:text-sm text-neutral-400 font-semibold ">
                 &copy; {new Date().getFullYear()} Nbg Construction. All rights reserved.
             </p>
             <div className="flex gap-4">
